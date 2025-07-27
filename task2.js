c1d1f1ee9b8d4a9c6d557e361d37441c44a82b80
Maria Zhdanova <mari.zhdanova.94@gmail.com>
2025-07-27 18:18:25 +0300

    Add task2.2 about 3 Lines from User

---
+++ task2.js
@@ -0,0 +1,5 @@
+let line1 = prompt("Enter the first line:");
+let line2 = prompt("Enter the second line:");
+let line3 = prompt("Enter the third line:");
+
+document.getElementById("output").innerText = `${line3} , ${line1} , ${line2}`;
