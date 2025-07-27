6957172de34b5f8207814fb27577b1a27301ced6
Maria Zhdanova <mari.zhdanova.94@gmail.com>
2025-07-27 18:55:14 +0300

    Add task4.4 about User's name

--- task2.3.js
+++ task2.3.js
@@ -3,7 +3,7 @@ let number = prompt("Enter a 5-digit number:");
 if (number.length === 5 && !isNaN(number)) {
   let digits = number.split('').join(' ');
   console.log(digits);
-  //alert(digits); // можна також показати в alert
+  alert(digits);
 } else {
   alert("Please enter a valid 5-digit number.");
 }
