79f2957627bc73d4f7748abc08099c48496df16f
Maria Zhdanova <mari.zhdanova.94@gmail.com>
2025-08-03 23:06:28 +0300

    Updated task2.3 about 5-digit number

--- task2.3.js
+++ task2.3.js
@@ -1,9 +1,13 @@
-let number = prompt("Enter a 5-digit number:");
+let number = parseInt(prompt("Enter a 5-digit number:"));
 
-if (number.length === 5 && !isNaN(number)) {
-  let digits = number.split('').join(' ');
-  console.log(digits);
-  alert(digits);
+if (isNaN(number) || number < 10000 || number > 99999) {
+  console.log("It is not a 5-digit number!");
 } else {
-  alert("Please enter a valid 5-digit number.");
+  let digit1 = parseInt(number / 10000);       // перша цифра
+  let digit2 = parseInt(number / 1000) % 10;   // друга
+  let digit3 = parseInt(number / 100) % 10;    // третя
+  let digit4 = parseInt(number / 10) % 10;     // четверта
+  let digit5 = number % 10;                    // п’ята
+
+  console.log(digit1, digit2, digit3, digit4, digit5);
 }
