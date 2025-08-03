39a2ed49398b066d50cac50f2538e9fb897947bd
Maria Zhdanova <mari.zhdanova.94@gmail.com>
2025-08-03 22:16:15 +0300

    Add task6.2 about Array

--- task6.1.js
+++ task6.1.js
@@ -5,25 +5,25 @@ if (isNaN(length) || length <= 0) {
 } else {
   let arr = [];
 
-  // Користувач вводить елементи масиву
+  // вводимо елементи масиву
   for (let i = 0; i < length; i++) {
     let value = parseInt(prompt("Enter number №" + (i + 1) + ":"));
     arr.push(value);
   }
 
-  // Зберігаємо копію до змін
+  // зберігаємо копію масиву
   let originalArr = arr.slice();
 
-  // Вивід початкового масиву
+  // виводимо початковий масив
   document.write("<p><strong>First Array:</strong> " + originalArr.join(",") + "</p>");
 
-  // Сортування масиву
+  // сортування масиву
   arr.sort(function(a, b) {
     return a - b;
   });
   document.write("<p><strong>Sorted Array:</strong> " + arr.join(",") + "</p>");
 
-  // Видалення з 2-го по 4-й елементи
+  // видалення з 2-го по 4-й елементи
   arr.splice(1, 3);
   document.write("<p><strong>Array after removing 2nd to 4th elements:</strong> " + arr.join(",") + "</p>");
 }
---
+++ task6.2.js
@@ -0,0 +1,63 @@
+let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// Змінні
+let positiveSum = 0;
+let positiveCount = 0;
+let positiveProduct = 1;
+let min = arr[0];
+let minIndex = 0;
+let max = arr[0];
+let maxIndex = 0;
+let negativeCount = 0;
+let oddPositiveCount = 0;
+let evenPositiveCount = 0;
+let evenPositiveSum = 0;
+let oddPositiveSum = 0;
+
+// Обробка масиву
+for (let i = 0; i < arr.length; i++) {
+let el = arr[i];
+
+if (el > 0) {
+    positiveSum += el;
+    positiveCount++;
+    positiveProduct *= el;
+
+    if (el % 2 === 0) {
+    evenPositiveCount++;
+    evenPositiveSum += el;
+    } else {
+    oddPositiveCount++;
+    oddPositiveSum += el;
+    }
+}
+if (el < 0) {
+    negativeCount++;
+}
+if (el < min) {
+    min = el;
+    minIndex = i;
+}
+if (el > max) {
+    max = el;
+    maxIndex = i;
+}
+}
+// Вивід результатів
+console.log("Сума додатних елементів:", positiveSum);
+console.log("Кількість додатних елементів:", positiveCount);
+console.log("Добуток додатних елементів:", positiveProduct);
+console.log("Мінімальний елемент:", min, "Індекс:", minIndex);
+console.log("Максимальний елемент:", max, "Індекс:", maxIndex);
+console.log("Кількість від’ємних елементів:", negativeCount);
+console.log("Кількість непарних додатних елементів:", oddPositiveCount);
+console.log("Кількість парних додатних елементів:", evenPositiveCount);
+console.log("Сума парних додатних елементів:", evenPositiveSum);
+console.log("Сума непарних додатних елементів:", oddPositiveSum);
+
+// Заміна всіх елементів на 0, крім найбільшого
+for (let i = 0; i < arr.length; i++) {
+if (i !== maxIndex) {
+    arr[i] = 0;
+}
+}
+console.log("Масив після заміни всіх елементів на 0, крім найбільшого:", arr);
