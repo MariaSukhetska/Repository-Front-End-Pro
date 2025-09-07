d122a3583923157acf354db3fc9a872508893e97
Maria Zhdanova <mari.zhdanova.94@gmail.com>
2025-09-07 19:20:07 +0300

    Added updated task7.3

--- task7.3.js
+++ task7.3.js
@@ -1,13 +1,15 @@
 function f() {
-for (let i = 0; i < 10; i++) {
-    let value = prompt('Enter the number > 100');
-    let num = Number(value);
-if (num > 100 || isNaN(num) || i===9) {
-    console.log(`User has entered ${value}`);
-    break;
-} else if (num <=100 ) {
-    console.log('Enter the correct number');
-}
-}
+
+    for (let i = 0; i < 10; i++) {
+        let value = prompt('Enter the number > 100');
+        let num = Number(value);
+        if (num > 100 || isNaN(num) || i === 9) {
+            console.log(`User has entered ${value}`);
+            break;
+        } else if (num <= 100) {
+            console.log('Enter the correct number');
+        }
+    }
 }
 f(); 
+
