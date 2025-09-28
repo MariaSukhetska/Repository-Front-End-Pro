63533279e7a476525edad7e3dd66f239d529671b
Maria Zhdanova <mari.zhdanova.94@gmail.com>
2025-09-28 19:01:11 +0300

    Added task12.1

---
+++ task12.1.js
@@ -0,0 +1,22 @@
+let savedLink = null;
+
+const setLinkBtn = document.getElementById("setLinkBtn");
+const goLinkBtn = document.getElementById("goLinkBtn");
+
+setLinkBtn.addEventListener("click", () => {
+    const userInput = prompt("Введіть посилання (URL):");
+    if (userInput) {
+        savedLink = userInput.trim();
+        alert(`Посилання збережено: ${savedLink}`);
+    } else {
+        alert("Посилання не введене!");
+    }
+});
+
+goLinkBtn.addEventListener("click", () => {
+    if (savedLink) {
+        window.location.href = savedLink; // переадресація
+    } else {
+        alert("Спочатку введіть посилання!");
+    }
+});
